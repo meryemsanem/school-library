b20428cc264063bc15c9423ee4969b4724ceb129
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-09-04 16:37:34 +0300

    Add code to see decorators in action

--- classes/trimmer_decorator.rb
+++ classes/trimmer_decorator.rb
@@ -3,6 +3,6 @@ require_relative 'decorator'
 class TrimmerDecorator < Decorator
   def correct_name
     name = @nameable.correct_name
-    name.length >= 10 ? name : name[0, 9]
+    name.length >= 10 ? name[0, 10] : name
   end
 end
