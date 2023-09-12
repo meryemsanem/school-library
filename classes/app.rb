d02ad1a07814bdfd63345c7509fc67e6074276d0
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-09-12 21:50:47 +0300

    Fix displaying rentals issue in terminal before quitting

--- classes/app.rb
+++ classes/app.rb
@@ -99,12 +99,12 @@ class App
     puts 'Enter the ID of the person: '
     person_id = gets.chomp.to_i
     puts 'Rentals: '
-    person_rentals = @rentals.select { |rental| rental[:person][:id] == person_id }
+   person_rentals = @rentals.select { |rental| rental.person.id == person_id }
     if person_rentals.empty?
       puts 'No rentals found for this person.'
     else
       person_rentals.each do |rental|
-        puts "Date: #{rental[:date]}, Book '#{rental[:book][:title]}' by #{rental[:book][:author]}"
+        puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}"
       end
     end
   end
