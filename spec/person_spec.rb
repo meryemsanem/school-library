c6527145a98769948c92e07727078be699867c6a
RileyManda <rileymanda69@gmail.com>
2023-09-13 10:08:40 +0200

    Person tests: correstly returns name,unique id and parent permission bolean

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,5 +6,19 @@ describe Person do
 
   it 'initializes with age, name, and parent permission' do
     expect(person.age).to eq(25)
+    expect(person.name).to eq('John')
+    expect(person.instance_variable_get(:@parent_permission)).to be_falsey
+  end
+
+  it 'correctly returns the name' do
+    expect(person.correct_name).to eq('John')
+  end
+
+  it 'initializes with a unique ID' do
+    expect(person.instance_variable_get(:@id)).to be_a(Integer)
+  end
+  it 'can use services with parent permission' do
+    person = Person.new(15, 'Bob', parent_permission: true)
+    expect(person.can_use_services?).to be_truthy
   end
 end
