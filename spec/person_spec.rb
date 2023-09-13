5314a0a1b9eab9303100a577387fbe8a55eb826d
RileyManda <rileymanda69@gmail.com>
2023-09-13 13:53:43 +0200

    Updated person tests to include:constructor method to return an empty rental|unknown name|

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -7,18 +7,46 @@ describe Person do
   it 'initializes with age, name, and parent permission' do
     expect(person.age).to eq(25)
     expect(person.name).to eq('John')
-    expect(person.instance_variable_get(:@parent_permission)).to be_falsey
+    expect(person.parent_permission).to be_falsey
+    expect(person.rentals).to eq([])
+  end
+
+  it 'initializes with a unique ID' do
+    expect(person.id).to be_a(Integer)
+  end
+
+  it 'initializes with default name "Unknown" if name is not provided' do
+    person_without_name = Person.new(30)
+    expect(person_without_name.name).to eq('unknown')
+  end
+
+  it 'can use services with parent permission' do
+    person_with_permission = Person.new(15, 'Bob', parent_permission: true)
+    expect(person_with_permission.can_use_services?).to be_truthy
+  end
+
+  it 'cannot use services without parent permission if under age' do
+    underage_person = Person.new(16, 'Alice', parent_permission: false)
+    expect(underage_person.can_use_services?).to be_falsey
+  end
+
+  it 'can use services without parent permission if over age' do
+    adult_person = Person.new(18, 'Eve', parent_permission: false)
+    expect(adult_person.can_use_services?).to be_truthy
   end
 
   it 'correctly returns the name' do
     expect(person.correct_name).to eq('John')
   end
 
-  it 'initializes with a unique ID' do
-    expect(person.instance_variable_get(:@id)).to be_a(Integer)
+  it 'does not correct the name' do
+    person_with_long_name = Person.new(30, 'alongnameiscorrected')
+    expect(person_with_long_name.correct_name).to eq('alongnameiscorrected')
   end
-  it 'can use services with parent permission' do
-    person = Person.new(15, 'Bob', parent_permission: true)
-    expect(person.can_use_services?).to be_truthy
+
+  it 'can add a rental and return it' do
+    rental = double('Rental')
+    expect(person.add_rental(rental)).to eq([rental])
+    expect(person.rentals).to include(rental)
   end
 end
