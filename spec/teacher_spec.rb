9eea593590569b939c5b199e85140c6aa5e84f88
RileyManda <rileymanda69@gmail.com>
2023-09-13 10:37:50 +0200

    Teacher test: can use services without parent permission

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require_relative '../classes/teacher'
+require_relative '../classes/person'
 
 describe Teacher do
   let(:teacher) { Teacher.new(30, 'Doctor', parent_permission: true) }
@@ -7,5 +8,13 @@ describe Teacher do
     expect(teacher.age).to eq(30)
     expect(teacher.specialization).to eq('Doctor')
     expect(teacher.instance_variable_get(:@parent_permission)).to be_truthy
+  end
+  it 'is a kind of Person' do
+    expect(teacher).to be_a(Person)
+  end
+
+  it 'can use services without parent permission' do
+    teacher_without_permission = Teacher.new(30, 'Doctor', parent_permission: false)
+    expect(teacher_without_permission.can_use_services?).to be_truthy
   end
 end
