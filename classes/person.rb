7a1aef5e9f4c73cee56b145bc4e1e35e22c12bec
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-09-13 13:12:08 +0300

    Add RSpec tests for the Student class

--- classes/person.rb
+++ classes/person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :age, :name
-  attr_reader :id, :rentals
+  attr_reader :id, :rentals, :parent_permission
 
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
